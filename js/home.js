4c2535b93dc5d565cbd15bb41aca21aebad92b61
Felisberto3 <felisbertoalberto183@gmail.com>
2023-12-05 16:26:15 +0100

    knelk

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
 const menuIcon = document.querySelector('.menu-Icon')
 console.log(menuIcon);
 menuIcon.onclick = () =>{
-        document.querySelector('.nav-list').style.display="block"
+        document.querySelector('nav').classList.toggle('showed_Nav')
 }
